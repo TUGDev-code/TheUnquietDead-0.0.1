8e825cbb4e82c238c3365c9dada36592b44ae00a
TUGDev-code <nightmarelabsstudio@gmail.com>
2025-06-24 16:22:09 +0500

    13

---
+++ Assets/ThunderWire Studio/UHFPS/Content/Scripts/Editor/Runtime/PlayerItems/FlashlightItemEditor.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEditor;
+using UHFPS.Runtime;
+using ThunderWire.Editors;
+
+namespace UHFPS.Editors
+{
+    [CustomEditor(typeof(FlashlightItem))]
+    public class FlashlightItemEditor : PlayerItemEditor<FlashlightItem>
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorDrawing.DrawInspectorHeader(new GUIContent("Flashlight Item"), Target);
+            EditorGUILayout.Space();
+
+            serializedObject.Update();
+            {
+                base.OnInspectorGUI();
+                EditorGUILayout.Space();
+
+                Properties.Draw("<ItemObject>k__BackingField");
+                Properties.Draw("BatteryInventoryItem");
+                Properties.Draw("FlashlightLight");
+                Properties.Draw("LightIntensity");
+                // Yangi xususiyatni chizish
+                EditorGUILayout.Space();
+                using (new EditorDrawing.BorderBoxScope(new GUIContent("New GameObject Settings")))
+                {
+                    Properties.Draw("onORof");
+                    Properties.Draw("Flashget");
+                }
+
+                EditorGUILayout.Space();
+                using (new EditorDrawing.BorderBoxScope(new GUIContent("Battery Settings")))
+                {
+                    Properties.Draw("InfiniteBattery");
+                    Properties.Draw("BatteryLife");
+                    Properties.Draw("BatteryPercentage");
+                    Properties.Draw("BatteryLowPercent");
+                    Properties.Draw("ReloadLightEnableOffset");
+                    Properties.Draw("BatteryFullColor");
+                    Properties.Draw("BatteryLowColor");
+
+                    EditorGUILayout.Space();
+                    float batteryEnergy = Application.isPlaying ? Target.batteryEnergy : Target.BatteryPercentage.Ratio();
+                    int batteryPercent = Mathf.RoundToInt(batteryEnergy * 100);
+                    Rect batteryPercentageRect = EditorGUILayout.GetControlRect();
+                    EditorGUI.ProgressBar(batteryPercentageRect, batteryEnergy, $"Battery Energy ({batteryPercent}%)");
+                }
+
+                EditorGUILayout.Space();
+                using (new EditorDrawing.BorderBoxScope(new GUIContent("Animation Settings")))
+                {
+                    Properties.Draw("FlashlightDrawState");
+                    Properties.Draw("FlashlightHideState");
+                    Properties.Draw("FlashlightReloadState");
+                    Properties.Draw("FlashlightIdleState");
+                    Properties.Draw("FlashlightHideTrim");
+
+                    EditorGUILayout.Space();
+                    EditorGUILayout.LabelField("Triggers", EditorStyles.boldLabel);
+                    Properties.Draw("FlashlightHideTrigger");
+                    Properties.Draw("FlashlightReloadTrigger");
+                }
+
+                EditorGUILayout.Space();
+                using (new EditorDrawing.BorderBoxScope(new GUIContent("Sound Settings")))
+                {
+                    Properties.Draw("FlashlightClickOn");
+                    Properties.Draw("FlashlightClickOff");
+                }
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
